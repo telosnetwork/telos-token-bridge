257add447b27d3d021bd629c866b8b6362259e84
tomtomcrypto <titiomm@gmail.com>
2022-10-08 17:32:50 +0000

    tests skeleton & misc

---
+++ evm/test/TokenBridgeRegister.js
@@ -0,0 +1,39 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const ONE_TLOS = ethers.utils.parseEther("1.0");
+const HALF_TLOS = ethers.utils.parseEther("0.5");
+const HUNDRED_TLOS = ethers.utils.parseEther('200.0');
+const TOKEN_NAME = "My Bridgeable Token";
+const TOKEN_SYMBOL = "MBT";
+const MAX_REQUESTS = 10;
+
+describe("TokenBridgeRegister Contract", function () {
+    let antelope_bridge, evm_bridge, user, erc20bridgeable, register;
+    beforeEach(async () => {
+        [antelope_bridge, user] = await ethers.getSigners();
+        let EVMBridge = await ethers.getContractFactory("TokenBridge");
+        evm_bridge = await EVMBridge.deploy(antelope_bridge.address, MAX_REQUESTS, HALF_TLOS);
+        let ERC20Bridgeable = await ethers.getContractFactory("ERC20Bridgeable");
+        erc20bridgeable = await ERC20Bridgeable.deploy(antelope_bridge.address, TOKEN_NAME, TOKEN_SYMBOL);
+        let TokenRegister = await ethers.getContractFactory("TokenBridgeRegister");
+        register = await TokenRegister.deploy(antelope_bridge.address, evm_bridge.address);
+    })
+    describe(":: Deployment", async function () {
+        it("Should have the right antelope bridge evm address" , async function () {
+            expect(await register.bridge()).to.equal(antelope_bridge.address);
+        });
+        it("Should have the right evm bridge address" , async function () {
+            expect(await register.bridge()).to.equal(evm_bridge.address);
+        });
+    });
+    describe(":: Token CRUD", async function () {
+        it("Should let both bridges add a token" , async function () {
+        });
+        it("Should not let other addresses add a token" , async function () {
+        });
+        it("Should not let other addresses remove a token" , async function () {
+        });
+        it("Should not let other addresses pause a token" , async function () {
+        });
+    });
+});
