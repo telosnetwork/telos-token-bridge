6bd6bd6689e60ed74ba52bd94a9c12bed5e402bc
Thomas Cuvillier <titiomm@gmail.com>
2022-10-27 00:44:51 +0200

    Delete antelope/escrow directory

--- antelope/escrow/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo ">>> Building contract..."
-if [ ! -d "$PWD/build" ]
-then
-  mkdir build
-fi
-eosio-cpp -I="./include/"  -I="./external/"  -o="./build/token.bridge.wasm" -contract="tokenbridge" -abigen ./src/token.bridge.cpp
--- antelope/escrow/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-echo ">>> Deploying contract..."
-if [ "$1" == "mainnet" ]
-then
-  url="https://mainnet.telos.caleos.io"
-else
-  url="https://testnet.telos.caleos.io"
-fi
-cleos --url "$url" set contract tokenbridge "$PWD/build" token.bridge.wasm token.bridge.abi
